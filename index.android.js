e47aec12386b56f61d23dcd573d83240a1f3045a
WBCOHBO <andy405641@gmail.com>
2017-07-06 11:18:31 +0800

    RNView

--- index.android.js
+++ index.android.js
@@ -1,91 +1,13 @@
-// /**
-//  * Sample React Native App
-//  * https://github.com/facebook/react-native
-//  * @flow
-//  */
-
-// import React, { Component } from 'react';
-// // rowHasChange.catch(function() {});
-// import {
-//   AppRegistry,
-//   StyleSheet,
-//   Text,
-//   View,
-//   ListView
-// } from 'react-native';
-
-// // ListView
-// class MyComponent extends Component {
-//   constructor() {
-//     super();
-//     const ds = new ListView.DataSource({rowHasChanged: (r1, r2) => r1 !== r2});
-//     this.state ={
-//       daaSource: ds.cloneWithRows(['row 1', 'row 2']),
-//     };
-//   }
-
-//   render(){
-//     return (
-//       <ListView
-//         dataSource={this.state.dataSource}
-//         renderRow={(rowData) => <Text>{rowData}</Text>} />
-//     );
-//   }
-// }
-
-// // text
-// // export default class AwesomeProject extends Component {
-// //   render() {
-// //     return (
-// //       <View>
-// //         <MyComponent text='I love to blink' />
-// //         <MyComponent text='Yes blinking is so great' />
-// //         <MyComponent text='Why did they ever take this out of HTML' />
-// //         <MyComponent text='Look at me look at me look at me' />
-// //       </View>
-// //     );
-// //   }
-// // }
-
-// // const styles = StyleSheet.create({
-// // baseText: {
-// //     fontFamily: 'Cochin',
-// //   },
-// //   titleText: {
-// //     fontSize: 20,
-// //     fontWeight: 'bold',
-// //   },
-// // });
-
-
-
-// // AppRegistry.registerComponent('AwesomeProject', () => AwesomeProject);
-
 import React, { Component } from 'react';
 import { AppRegistry, FlatList, StyleSheet, Text, View } from 'react-native';
 
 export default class FlatListBasics extends Component {
-
-  item = (props) => {
-    return  <Text style={styles.item}>{props.item.key}</Text>
-  }
   render() {
     return (
-      <View style={styles.container}>
-        <FlatList
-          data={[
-            {key: '123'},
-            {key: '312'},
-            {key: 'bbb'},
-            {key: 'aaa'},
-            {key: 'qqq'},
-            {key: 'ddd'},
-            {key: 'fff'},
-            {key: 'zzz'},
-          ]}
-          renderItem={this.item}
-        />
-      </View>
+    <View style={{ flex: 1 }}>
+    <View style={{backgroundColor: 'cadetblue', flex: 1}} />
+    <View style={{backgroundColor: 'blue', flex: 1}} />
+    </View>
     );
   }
 }
