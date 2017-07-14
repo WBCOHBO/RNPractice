db31766d08af2265d36ca684d90e5f611aa1928a
WBCOHBO <andy405641@gmail.com>
2017-07-14 13:13:06 +0800

    ListViewTextOK

--- src/ButtonSample.js
+++ src/ButtonSample.js
@@ -10,18 +10,23 @@ import {
 } from 'react-native';
 
 export default (props) => (
+  // console.log("!!!!!!!", props)
+  // return (
   <View style={{flex: 1}}>
     <TouchableOpacity style={styles.button}>
       <Image style={{width: 50, height: 50,borderRadius: 30}}
           source={{uri: 'https://facebook.github.io/react/img/logo_og.png'}}
       />
-      <Text style={styles.buttonText}>{props.title}</Text>
-      <Text style={styles.buttonText}>{props.desc}</Text>
+      <View>
+      <Text style={styles.title}>{props.buttonText.title}</Text>
+      <Text style={styles.desc}>{props.buttonText.desc}</Text>
+      </View>
       <Image style={{ width: 30, height: 30,top:15,left:380,position: 'absolute',}}
           source={{uri: 'https://cdn2.iconfinder.com/data/icons/flat-and-simple-part-4/128/cross-128.png'}}
       />
     </TouchableOpacity>
   </View>
+// );
 );
 
 
@@ -35,7 +40,7 @@ const styles = StyleSheet.create({
   // },
    button: {
     flexDirection: 'row',
-    margin: 1,
+    margin: 3,
     paddingTop: 10,
     paddingBottom: 10,
     // paddingLeft: 15,
@@ -46,6 +51,7 @@ const styles = StyleSheet.create({
     justifyContent: 'flex-start',
    },
   title: {
+    flexDirection: 'column',
     color: 'black',
     fontSize: 20,
     fontWeight: 'bold',
@@ -54,8 +60,9 @@ const styles = StyleSheet.create({
     // backgroundColor:'black',
   },
   desc:{
-    color: 'black',
-    fontSize: 15,
+    flexDirection: 'column',
+    color: 'gray',
+    fontSize: 18,
     paddingLeft: 20,
   }
 });
